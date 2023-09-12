2bf1726400378a1656ee2313be684050ed4e6436
BihikovDev <bihikovnatio@icloud.com>
2023-09-12 14:02:34 +0200

    Ajout page d'articles

---
+++ views/pages.php
@@ -0,0 +1,19 @@
+<section class="article container-fluid text-center text-justify">
+    <div class="row">
+        <div class="firstSubject col-12 col-md-12">
+            <p class="articleDate">12/09/2023</p>
+            <h2 class="articleTitle">Titre de l'article</h2>
+            <img class="img-fluid col-12 col-md-6" src="/public/assets/img/imgtest.jpg" alt="">
+            <p class="articleText">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Dignissimos quidem assumenda facilis id! Alias doloribus vitae labore reiciendis delectus. 
+                Labore hic reprehenderit, repellat minus laudantium voluptas voluptatibus! 
+                Mollitia, fugit eaque quidem illum earum quae amet a distinctio sed delectus quisquam 
+                reprehenderit consequatur tenetur. Voluptate eos eveniet obcaecati maxime ab, 
+                fugit ad hic sit tempore nisi vel soluta dignissimos eligendi totam a accusamus eius, 
+                libero delectus fuga? Perferendis, ipsa cum, nostrum officia molestiae sed ipsam aliquid in 
+                illo et pariatur, quia quam laborum nisi ex dolorem eveniet? Repellat earum, 
+                et quibusdam ab voluptas minus quod totam quam, voluptates fugit laboriosam voluptatibus?</p>
+                <a href=""><button class="btn mb-3">Lire l'article</button></a>
+        </div>
+    </div>
+</section>
