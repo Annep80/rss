1cc3772b2ea1dd62c621b1132b3e7146b40f9c19
SebastienLer <leroy.sebast@orange.fr>
2023-09-12 10:29:33 +0200

    accueil proto

---
+++ controllers/follow-ctrl.php
@@ -0,0 +1,5 @@
+<?php 
+$sports = ['Football', 'Tennis', 'Rugby','Golf','Hanball'];
+include __DIR__ . '/../views/templates/header.php';
+include __DIR__ . '/../views/follow.php';
+include __DIR__ . '/../views/templates/footer.php';
