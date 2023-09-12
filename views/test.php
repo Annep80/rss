ebd476b53a7154d1af251e83230aca13d38dfa15
SebastienLer <leroy.sebast@orange.fr>
2023-09-12 15:12:03 +0200

    test flux rss

---
+++ views/test.php
@@ -0,0 +1,32 @@
+<?php 
+echo '<div class="card mt-3">';
+foreach ($rss->channel->item as $item){
+    // var_dump($item);
+    // die;
+    $titleItem = $item->title;
+    $imgItem = $item->enclosure['url'];
+    $descriptionItem = $item->description;
+    $descriptionItem = preg_replace("/<img[^>]+\>/i", "", $descriptionItem); 
+    // echo($imgItem); 
+    // die;
+    $datetime = date_create($item->pubDate);
+    $date = date_format($datetime, 'd M Y, H\hi');
+    echo 
+    '<h5 class="card-title">'.$titleItem.'</h5>
+    <img class="col-6" src="'.$imgItem.'" alt="'.$imgItem.'" />
+    <div class="card-body">
+        <p>'.$descriptionItem.'</p>
+        <a href="'.$item->link.'">'.mb_convert_encoding($item->title,'UTF-8').'</a> ('.$date.')
+    </div>';
+}
+echo '</div>';
+?>
+
+            <!-- <div class="card mt-3">    
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Exemple de titre</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Lire</a>
+                </div>
+            </div> -->
