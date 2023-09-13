f3195f6c5751fe854e22ae0cf0c68526d2f9f1c1
BihikovDev <bihikovnatio@icloud.com>
2023-09-13 16:35:43 +0200

    Maj cookie

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (empty($_COOKIE)) {
+if (empty($_COOKIE['sportChoice'])) {
     header('location: controllers/parameters-ctrl.php');
 } else {
     header('location: controllers/home-ctrl.php');
