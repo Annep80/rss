c567a2bf0132ef9d93c9e61a459486f32f54e764
SebastienLer <leroy.sebast@orange.fr>
2023-09-13 09:07:39 +0200

    correct card

--- views/test.php
+++ views/test.php
@@ -11,15 +11,7 @@ foreach ($rss->channel->item as $item){
     $datetime = date_create($item->pubDate);
     $date = date_format($datetime, 'd M Y, H\hi');
     echo 
-
-    '<div class="card mt-3">
-        <h5 class="card-title mb-3">'.$titleItem.'</h5>
-        <img class="col-6" src="'.$imgItem.'" alt="'.$imgItem.'" />
-        <div class="card-body">
-            <p class="card-text">'.$descriptionItem.'</p>
-            <a href="'.$item->link.'">'.mb_convert_encoding($item->title,'UTF-8').'</a> ('.$date.')
-
-    <div class="col-12 col-md-4 card mt-3">
+    '<div class="col-12 col-md-4 card mt-3">
         <img class="card-img-top col-6" src="'.$imgItem.'" alt="'.$imgItem.'" />
         <h5 class="mt-3 card-title">'.$titleItem.'</h5>
         <div class="card-body">
