e1a2db1f87cdd7cbc712620fffa6ac0627922577
SebastienLer <leroy.sebast@orange.fr>
2023-09-14 10:52:32 +0200

    correctif div

--- views/choice1.php
+++ views/choice1.php
@@ -15,14 +15,24 @@ foreach ($rss1->channel->item as $item) {
     
     if($display < $displayChoice){
     echo 
-    '<div class="card mt-3">
-        <img class="card-img-top h-25 col-6" src="'.$imgItem.'" alt="'.$imgItem.'" />
+    '<div class="card mt-3 row">
+        <div class="col-12">
+        <img class="card-img-top " src="'.$imgItem.'" alt="'.$imgItem.'" />
+        </div>
+        <div class="col-12">
         <h5 class="card-title mb-5 mt-4">'.$titleItem.'</h5>
-        <div class="card-body h-50">
-            ('.$date.') 
-            <p class="h-50 overflow-hidden">'.$descriptionItem.'</p>
-            <div class="buttonCard mt-5"> 
-            <a href="' . $item->link . '"class="btn btn-primary" target="blank">Accèder à l\'article</a>
+        </div>
+        <div class="card-body col-12">
+            <div class="row justify-content-end">
+                <div class="col-12">
+                    <p>('.$date.')</p>
+                </div>
+                <div class="col-12"> 
+                    <p>'.$descriptionItem.'</p>
+                </div>
+                <div class="buttonCard mt-5 col-12 d-flex justify-content-end"> 
+                    <a href="' . $item->link . '"class="btn btn-primary" target="blank">Accèder à l\'article</a>
+                </div>
             </div>
         </div>
     </div>';
