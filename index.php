739a1aaf8ee9ff8ca54908d34e11f1488016dedd
BihikovDev <bihikovnatio@icloud.com>
2023-09-14 10:06:07 +0200

    Un push

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (empty($_COOKIE['sportChoice'])) {
+if (empty($_COOKIE['sportChoosen'])) {
     header('location: controllers/parameters-ctrl.php');
 } else {
     header('location: controllers/home-ctrl.php');
